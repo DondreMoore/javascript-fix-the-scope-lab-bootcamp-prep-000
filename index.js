39507708274eff9cdb8eed608b1ee9401759faf4
Dondre Moore <raptureimmortal@icloud.com>
2019-03-03 01:15:16 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,27 @@
+var animal = 'dog'
+
+function myAnimal() {
+  return animal
+}
+
+function yourAnimal() {
+  var animal = 'cat'
+  return animal
+}
+
+function add2(n) {
+  return n += 2 ;
+}
+
+var funkyFunction = function() {
+  return function() {
+    return "FUNKY!"
+  }
+}
+
+
+var theFunk = funkyFunction() {
+function theFunk() {
+  return function()
+}
+}
